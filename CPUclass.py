69f168d0b96d6f738671df8081fd21abb74403eb
Starwolf16 <jmtarrant00@gmail.com>
2022-11-29 23:12:50 -0500

    Update CPUclass.py

--- CPUclass.py
+++ CPUclass.py
@@ -1,26 +1,28 @@
 from random import sample 
 from time import time, sleep
 from math import floor
+from copy import deepcopy
 
 
 class CPU:
     RAM = 24
     startTime = 0
+    sortList = list()
     def __init__(self, memory, sortList) -> None:
         self.memory = memory
-        self.sortList = sortList
+        self.sortList = deepcopy(sortList)
         
     def makeArray(self, infection):
         multiplier = 1 if infection < 1 else infection
         # print(multiplier)
         nums = sample(range(0,10000*multiplier), (9000*multiplier))
         for x in range(len(nums)):
-            sortList.append(nums[x])
+            self.sortList.append(nums[x])
 
-        size = len(sortList)
+        size = len(self.sortList)
 
         self.startTime = time()
-        self.quickSort(sortList, 0, size - 1)
+        self.quickSort(self.sortList, 0, size - 1)
         # print("Array Sorted!")    
 
     def partition(self, array, low, high):
@@ -57,44 +59,56 @@ class CryptoJacker:
         return int(self.cryptoRam)
     
     def mineCoins(self, seconds):
-        coinsPerSec = self.cryptoRam * 0.05
-        self.coinsMined = coinsPerSec * seconds
-    
-    def printCoins(self):
-        print(f"Mined {self.coinsMined} ShiCoins")
-        
+        coinsPerSec = float(self.cryptoRam) * 0.05
+        self.coinsMined += coinsPerSec * seconds
+        print(f"Total ShiCoins mined: {self.coinsMined}")    
+     
+
+class Detector:
+    def __init__(self) -> None:
+        pass
+
+    def detect(self, expectedTime, actualTime):
+        if actualTime != expectedTime:
+            print("\n-------------------------------------------\n" + 
+                  "  WARNING: CRYPTOJACKER RUNNING ON SYSTEM\n" + 
+                  "-------------------------------------------\n" + 
+                  f"Expected Execution Time: {expectedTime}\n" + 
+                  f"Actual Execution Time: {actualTime}")
 
 def Menu():
-    print("---Main Menu---\n" +  
+    print("\n------Main Menu------\n" +  
           "1. Run Program\n" + 
           "2. Exit Program\n")
-    return int(input("Enter an Option: "))
+    return int(input("Enter an Option: ") or 0)
 
 
 run = True
 sortList = list()
-cpu1 = CPU(10, sortList)
+cpu = CPU(10, sortList)
 crypto = CryptoJacker()
 
-cpu1.makeArray(0)
+cpu.makeArray(0)
+expectedTime = time() - cpu.startTime
 
 while(run):
-    input = Menu()
-    if input == 1: 
+    userInput = Menu()
+    if userInput == 1: 
+        del cpu
+        cpu = CPU(10, sortList)
+        detector = Detector()
         stolen = crypto.stealRAM()
-        infection = floor((stolen / cpu1.RAM) * 250)
-        cpu1.makeArray(infection)
-        time = time() - cpu1.startTime
-        crypto.mineCoins(time)
-        formatTime = "{:.2f}".format(time)
+        infection = floor((stolen / cpu.RAM) * 250)
+        cpu.makeArray(infection)
+        totalTime = time() - cpu.startTime
+        crypto.mineCoins(totalTime)
+        formatTime = "{:.2f}".format(totalTime)
         print(f"Exectution Time: {formatTime} sec")
-        crypto.printCoins()
-        break
+        detector.detect("{:.2f}".format(expectedTime), formatTime)
 
-    elif input == 2:
+    elif userInput == 2:
         print("Done! Thank you")
         run = False
-        break
 
     else: 
         print("Please input a valid menu option (1 or 2)")
